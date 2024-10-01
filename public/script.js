7c0107bbf66ad78cc3206ba11a0a4c805575092b
NatashaAbdul <sitinatashaabdulkadir@gmail.com>
2024-10-01 09:30:52 +0800

    cleaned up codes

--- public/script.js
+++ public/script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     scannerInput.focus();
 
     let inputTimeout; // Variable to hold the timeout ID
-    const delay = 2000; // Delay time in milliseconds (e.g., 1 second)
+    const delay = 1000; // Delay time in milliseconds (e.g., 1 second)
     
     scannerInput.addEventListener('input', (event) => {
         const scannedData = event.target.value.trim();
@@ -29,29 +29,12 @@ document.addEventListener('DOMContentLoaded', () => {
                 link.width = "100%"; // You can adjust width/height as needed
                 link.height = "100%"; // Adjust as needed
 
-                // // Append the iframe to the body (or another container element)
-                // document.getElementsByClassName('container')[0].style.display = 'none'; // Replace 'some-class' with the actual class name to remove
+                // Append the iframe to the body (or another container element)
                 document.body.appendChild(link);
-
-                // console.log(link)
-                // link.rel = "noopener noreferrer"
-                // document.body.appendChild(link).click();
-                // window.open(scannedData, '_blank'); // Open the URL in a new tab
                 event.target.value = ''; // Clear the input field
             }else{
                 event.target.value = ''; // Clear the input field
             }
         }, delay); // The delay period before opening the new tab
     });
-
-    // // If you want to handle the Enter key press
-    // scannerInput.addEventListener('keydown', (event) => {
-    //     if (event.key === 'Enter') {
-    //         const scannedData = scannerInput.value.trim();
-    //         if (scannedData) {
-    //             window.open(scannedData, '_blank');
-    //             scannerInput.value = '';
-    //         }
-    //     }
-    // });
 });
